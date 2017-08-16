9885fa2a8c5bdc9a663b8b59122b32cf11c4f3c2
Morkeltry <morkeltry@mailfence.com>
2017-08-16 16:14:12 +0100

    added partials : header, footer, htmlHead; added main.hbs (not final); edited routes in controllers/index.js; began results.js Relates #11, #12, #13, #14, #15, 30

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -1,4 +1,18 @@
-searchResults.get = (req, res) => {
+const searchResults = {}
+
+listOfPlanets = ({querydetails}) => {
+  if (querydetails.searchAll)
+    const dbQuerySelectCriterion = {query: "", params: null};
+  else {
+    const dbQuerySelectCriterion = {query: "WHERE distance <= {$1}", params: [querydetails.distance]};
+  }       ///creates the SECOND HALF of a SELECT query, prepared in pg's parameterising format
+  /// do a db query on details, or on
+
+}
+
+
+searchResults.get = (req, res) => {    ///can this take a thrid argument which would be :distance ?
+  /// do a db query on details, or on /all , using listOfPlanets({distance:x})
 
 };
 
