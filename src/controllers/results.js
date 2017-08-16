75f0cea04fb19fb35844df18c166fc33440d96c6
Morkeltry <morkeltry@mailfence.com>
2017-08-16 18:01:20 +0100

    added ORDER BY interest in listOfPlanets in controllers/results.js

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -2,9 +2,9 @@ const searchResults = {}
 
 listOfPlanets = (queryDistance) => {
   if (queryDistance === 'all')
-    const dbQuerySelectCriterion = {query: "", params: null};
+    const dbQuerySelectCriterion = {query: ";", params: null};
   else {
-    const dbQuerySelectCriterion = {query: "WHERE distance <= {$1}", params: [queryDistance]};
+    const dbQuerySelectCriterion = {query: "WHERE distance <= {$1} ORDER BY interest;", params: [queryDistance]};
   }       ///creates the SECOND HALF of a SELECT query, prepared in pg's parameterising format
   /// do a db query on details, or on
 
