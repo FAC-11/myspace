c5ef4d6cc0f9f1778adaba5235f61b8870a91dca
RogeredBacon <jinglis12@googlemail.com>
2017-08-16 17:04:19 +0100

    fixed database sql slightly and added planning pic #33

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -10,25 +10,27 @@ distance      FLOAT(3)      NOT NULL,
 description   TEXT          NOT NULL,
 temperature   INTEGER       NOT NULL,
 weather       VARCHAR(200)  NOT NULL,
-interest      INTEGER       DEFAULT 0
+interest      INTEGER       DEFAULT 0,
+filename      varchar(20)   NOT NULL,
+thumbnailname varchar(20)   NOT NULL
 );
 
 
-INSERT INTO planets (name, distance, description, temperature, weather, interest) VALUES
-('Sol', '0', 'description of Earth', '5000000', 'weather report', 1),
-('Mercury', '0.61', 'description of Mercury', '272', 'weather report', 50),
-('Venus', '0.28', 'description of Venus', '737', 'weather report', 40),
-('Earth', '0', 'description of Earth', '288', 'weather report', 50),
-('The Moon', '0.003', 'description of Earth', '185', 'weather report', 20),
-('Mars', '0.52', 'description of Mars', '210', 'weather report', 4),
-('Jupiter', '4.2', 'description of Jupiter', '165', 'weather report', 20),
-('Io', '4.2', 'description of IO', '110', 'weather report', 9),
-('Europa', '4.2', 'description of Europa', '102', 'weather report', 90),
-('Ganymede', '4.2', 'description of Ganymede', '110', 'weather report', 80),
-('Callisto', '4.2', 'description of Callisto', '134', 'weather report', 70),
-('Saturn', '8.52', 'description of Saturn', '134', 'weather report', 60),
-('Uranus', '18.21', 'description of Uranus', '76', 'weather report', 80),
-('Neptune', '29.09', 'description of Neptune', '72', 'weather report', 10);
+INSERT INTO planets (name, distance, description, temperature, weather, interest, filename, thumbnailname) VALUES
+('Sol', '0', 'description of Earth', '5000000', 'weather report', 1, '../pics/', '../pics/'),
+('Mercury', '0.61', 'description of Mercury', '272', 'weather report', 50, '../pics/', '../pics/'),
+('Venus', '0.28', 'description of Venus', '737', 'weather report', 40, '../pics/', '../pics/'),
+('Earth', '0', 'description of Earth', '288', 'weather report', 50, '../pics/', '../pics/'),
+('The Moon', '0.003', 'description of Earth', '185', 'weather report', 20, '../pics/', '../pics/'),
+('Mars', '0.52', 'description of Mars', '210', 'weather report', 4, '../pics/', '../pics/'),
+('Jupiter', '4.2', 'description of Jupiter', '165', 'weather report', 20, '../pics/', '../pics/'),
+('Io', '4.2', 'description of IO', '110', 'weather report', 9, '../pics/', '../pics/'),
+('Europa', '4.2', 'description of Europa', '102', 'weather report', 90, '../pics/', '../pics/'),
+('Ganymede', '4.2', 'description of Ganymede', '110', 'weather report', 80, '../pics/', '../pics/'),
+('Callisto', '4.2', 'description of Callisto', '134', 'weather report', 70, '../pics/', '../pics/'),
+('Saturn', '8.52', 'description of Saturn', '134', 'weather report', 60, '../pics/', '../pics/'),
+('Uranus', '18.21', 'description of Uranus', '76', 'weather report', 80, '../pics/', '../pics/'),
+('Neptune', '29.09', 'description of Neptune', '72', 'weather report', 10, '../pics/', '../pics/');
 
 
 CREATE TABLE reviews (
