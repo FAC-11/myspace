9598aaed3f594fb735c3f8bc46c7b93884f705db
RogeredBacon <jinglis12@googlemail.com>
2017-08-17 11:47:53 +0100

    Started writing planet.hbs and changed index.js in model

--- src/model/index.js
+++ src/model/index.js
@@ -1,15 +1,18 @@
 module.exports= [
-'Sol',
-'Mercury',
-'Venus',
-'Earth',
-'The Moon',
-'Mars',
-'Jupiter',
-'Europa',
-'Ganymede',
-'Callisto',
-'Saturn',
-'Uranus',
-'Neptune'
+  {'Sol', '1', 'description of Earth', '5000000', 'weather report', 1, '/images/planets/', '../pics/'},
+  {'Mercury', '0.61', 'description of Mercury', '272', 'weather report', 50, '/images/planets/mercury.jpg', '../pics/'},
+  {'Venus', '0.28', 'description of Venus', '737', 'weather report', 40, '../pics/', '../pics/'},
+  {'Earth', '0', 'description of Earth', '288', 'weather report', 50, '../pics/', '../pics/'},
+  {'The Moon', '0.003', 'description of Earth', '185', 'weather report', 20, '../pics/', '../pics/'},
+  {'Mars', '0.52', 'description of Mars', '210', 'weather report', 4, '../pics/', '../pics/'},
+  {'Jupiter', '4.2', 'description of Jupiter', '165', 'weather report', 20, '../pics/', '../pics/'},
+  {'Io', '4.2', 'description of IO', '110', 'weather report', 9, '../pics/', '../pics/'},
+  {'Europa', '4.2', 'description of Europa', '102', 'weather report', 90, '../pics/', '../pics/'},
+  {'Ganymede', '4.2', 'description of Ganymede', '110', 'weather report', 80, '../pics/', '../pics/'},
+  {'Callisto', '4.2', 'description of Callisto', '134', 'weather report', 70, '../pics/', '../pics/'},
+  {'Saturn', '8.52', 'description of Saturn', '134', 'weather report', 60, '../pics/', '../pics/'},
+  {'Uranus', '18.21', 'description of Uranus', '76', 'weather report', 80, '../pics/', '../pics/'},
+  {'Neptune', '29.09', 'description of Neptune', '72', 'weather report', 10, '../pics/', '../pics/'}
 ];
+
+//planets (name, distance, description, temperature, weather, interest, filename, thumbnailname)
