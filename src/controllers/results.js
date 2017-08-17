208c60b2eb0a5f22667ca484db9912a234a769d0
Morkeltry <morkeltry@mailfence.com>
2017-08-17 13:04:36 +0100

    pointed CB chain route /results/xxx in controllers/index.js <-> c/results.js <-> model/queries/search-results. whole lot calls render on results.hbs Refer #38 #30

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -4,10 +4,10 @@ const searchResults = require ('../model/queries/search-query')
 const getPlanets = (req, res) => {
   const {distance} = req.params;
   searchResults.onDistance (distance, (err, planetsList)=> {
-    if (err) {console.log(err)}
+    if (err) {console.log('ERROR: ',err)}
     else {
-      console.log('LOG:', planetsList);
-      //do this with the results;
+      // console.log('LOG:', planetsList);
+      res.render ('results', planetsList);
     }
 
   });
--- src/model/queries/search-query.js
+++ src/model/queries/search-query.js
@@ -5,18 +5,6 @@ const searchResults = {};
 
 const queryFirstHalf = "SELECT * FROM planets";
 
-//
-// const fullQuery = (queryFirstHalf, queryDistance) => {
-//   let dbQuerySelectCriterion = {};
-//   if (queryDistance === 'all')
-//     dbQuerySelectCriterion = {query: queryFirstHalf+";", params: null};
-//   else {
-//     dbQuerySelectCriterion =
-//       {query: queryFirstHalf+"WHERE distance <= {$1} ORDER BY interest;", params: [queryDistance]};
-//   }
-//   console.log( dbQuerySelectCriterion);
-//   return dbQuerySelectCriterion;
-// }
 
 const fullQuery = (queryDistance) => {
   let dbQuerySelectCriterion = {};
@@ -24,19 +12,18 @@ const fullQuery = (queryDistance) => {
     dbQuerySelectCriterion = {secondHalf:";" , params: null};
   else {
     dbQuerySelectCriterion =
-      {secondHalf: "WHERE distance <= {$1} ORDER BY interest;", params: [queryDistance]};
+      {secondHalf: " WHERE distance <=$1 ORDER BY interest;", params: [Number(queryDistance)]};
   }
-  console.log( dbQuerySelectCriterion);
   return dbQuerySelectCriterion;
 }
 
 
-searchResults.onDistance = (distance, cb) => {  /// previously searchResults.get
-  query=fullQuery(distance)
+searchResults.onDistance = (distance, cb) => {
+  query=fullQuery(distance);
   dbConnection.query (queryFirstHalf+query.secondHalf, query.params, (err,res) => {
     if (err) cb(err);
     else {
-      cb (res.rows);
+      cb (null, res.rows);
     }
   });
 };
