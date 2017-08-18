b2cf5272b3f96c7aa81532690c0753f92224bb30
ameliejyc <amelie.chan@gmail.com>
2017-08-18 08:38:14 +0100

    amended styling and now works on error page relates #5

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -2,7 +2,7 @@ exports.client = (req, res) => {
   res.status(404).render('error', {
     layout: 'error',
     statusCode: 404,
-    errorMessage: 'Planet not found',
+    errorMessage: 'Pluto not found',
   });
 };
 exports.server = (err, req, res, next) => {
