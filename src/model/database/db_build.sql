b11f1c8b757406eb8ce4cf4558c8817ea3279fd2
ameliejyc <amelie.chan@gmail.com>
2017-08-21 15:18:24 +0100

    added in new photos

--- src/model/database/db_build.sql
+++ src/model/database/db_build.sql
@@ -20,17 +20,17 @@ INSERT INTO planets (name, distance, description, temperature, weather, interest
 ('Sol', '1', 'description of Earth', '5000000', 'weather report', 1, '/images/planets/sol.gif', '/images/planets/..pic'),
 ('Mercury', '0.61', 'description of Mercury', '272', 'weather report', 50, '/images/planets/mercury2.gif', '../pics/'),
 ('Venus', '0.28', 'description of Venus', '737', 'weather report', 40, '/images/planets/venus2.jpg', '../pics/'),
-('Earth', '0', 'description of Earth', '288', 'weather report', 50, '/images/planets/earth.gif', '../pics/'),
-('The Moon', '0.003', 'description of Earth', '185', 'weather report', 20, '../pics/', '../pics/'),
-('Mars', '0.52', 'description of Mars', '210', 'weather report', 4, '/images/planets/mars.gif', '../pics/'),
-('Jupiter', '4.2', 'description of Jupiter', '165', 'weather report', 20, '/images/planets/jupiter.gif', '../pics/'),
-('Io', '4.2', 'description of IO', '110', 'weather report', 9, '../pics/', '../pics/'),
+('Earth', '0', 'description of Earth', '288', 'weather report', 50, '/images/planets/earth2.jpg', '../pics/'),
+('The Moon', '0.003', 'description of Earth', '185', 'weather report', 20, '/images/planets/moon.jpg', '../pics/'),
+('Mars', '0.52', 'description of Mars', '210', 'weather report', 4, '/images/planets/mars2.jpg', '../pics/'),
+('Jupiter', '4.2', 'description of Jupiter', '165', 'weather report', 20, '/images/planets/jupiter2.jpg', '../pics/'),
+('Io', '4.2', 'description of IO', '110', 'weather report', 9, '/images/planets/io.jpg', '../pics/'),
 ('Europa', '4.2', 'description of Europa', '102', 'weather report', 90, '/images/planets/europa.jpg', '../pics/'),
 ('Ganymede', '4.2', 'description of Ganymede', '110', 'weather report', 80, '/images/planets/ganymede.jpg', '../pics/'),
 ('Callisto', '4.2', 'description of Callisto', '134', 'weather report', 70, '/images/planets/callisto.jpg', '../pics/'),
-('Saturn', '8.52', 'description of Saturn', '134', 'weather report', 60, '/images/planets/saturn.gif', '../pics/'),
-('Uranus', '18.21', 'description of Uranus', '76', 'weather report', 80, '/images/planets/uranus.gif', '../pics/'),
-('Neptune', '29.09', 'description of Neptune', '72', 'weather report', 10, '/images/planets/neptune.gif', '../pics/');
+('Saturn', '8.52', 'description of Saturn', '134', 'weather report', 60, '/images/planets/saturn2.jpg', '../pics/'),
+('Uranus', '18.21', 'description of Uranus', '76', 'weather report', 80, '/images/planets/uranus2.jpg', '../pics/'),
+('Neptune', '29.09', 'description of Neptune', '72', 'weather report', 10, '/images/planets/neptune2.jpg', '../pics/');
 
 
 CREATE TABLE reviews (
